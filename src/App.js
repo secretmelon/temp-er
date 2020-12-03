d51eb0b6da7933e43b6d87e1566ccfc4ef31f817
Melina Ariyani - WLG <Melina.Ariyani@NZTE-0KML85.local>
2020-12-03 14:51:50 +1300

    update styling

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ const App = () => {
     const [mainGif, setMainGif] = useState('');
     const [desc, setDesc] = useState('');
     const [homePageSet, setHomePageBool] = useState(false);
+    const [surprised, setSurprise] = useState(false);
 
+    // Only set up the landing page gif once
     if(!homePageSet){
-        getMainGif();
+        getMainGifArray();
         setHomePageBool(true);
     }
     
@@ -20,30 +22,38 @@ const App = () => {
         if(mainGif && homePageSet){
             const container = document.querySelector(".main-container");
             container.style.backgroundImage = `url(${mainGif})`;
-            console.log("11111")
         }
     }, [mainGif, homePageSet])
+
 
     const search = async (e) => {
         if(e.key === 'Enter'){
             const data = await fetchWeather(query)
             setWeather(data);
             setQuery('');
-            getGif(data);
+            getWeatherGif(data);
             getDesc(data);
         }
     }
+
+
 
     useEffect(() => {
         if (gif) {
             const welcomeContainer = document.querySelector(".main-header");
             welcomeContainer.style.display='none';
+            const surpriseMe = document.querySelector(".surprise");
+            surpriseMe.style.display='none';
+            const tryAnother = document.querySelector(".back");
+            tryAnother.style.display='flex';
             const container = document.querySelector(".main-container");
             container.style.backgroundImage = `url(${gif})`;
         }
     }, [gif])
 
-    async function getGif(data){
+    
+    // Retrieves a gif from Giphy API that corresponds with the weather description
+    async function getWeatherGif(data){
         try{
             const API_KEY = 'knQTvFoOegije3rg120nn19uFAsS7tz4';
             const BASE_URL = 'https://api.giphy.com/v1/gifs/search';
@@ -59,6 +69,7 @@ const App = () => {
           }
     }
 
+    // Searches for random gif with term 'Weather' for landing page
     async function getMainGif(){
         try{
             const API_KEY = 'knQTvFoOegije3rg120nn19uFAsS7tz4';
@@ -68,12 +79,41 @@ const App = () => {
             const gifCount = gifs.length;
             const randomIdx = Math.floor(Math.random() * gifCount);
             const gifUrl = gifs[randomIdx].images.downsized.url;
-            setMainGif(gifUrl);
+            // setMainGif(gifUrl);
           } catch (error) {
             console.warn(error);
           }
     }
 
+    // Picks a random gif from an array of pre-selected gifs for landing page
+    async function getMainGifArray(){
+        try{
+            const gifArray = [
+                'https://media.giphy.com/media/5W5TOAKuoZfa0/giphy.gif',
+                'https://media.giphy.com/media/MS0az8du4jir6/giphy.gif',
+                'https://media.giphy.com/media/xUPGcdhiQf2vbfDCyk/giphy.gif',
+                'https://media.giphy.com/media/qB6q1LG042M4o/giphy.gif',
+                'https://media.giphy.com/media/3o7TKGsK4gcsly0rW8/giphy.gif',
+                'https://media.giphy.com/media/yGs66ASVCCPQc/giphy.gif',
+                'https://media.giphy.com/media/BvQflwM7aDXva/giphy.gif',
+                'https://media.giphy.com/media/3D2EHbKSPAReFx7GGn/giphy.gif',
+                'https://media.giphy.com/media/l1Joik8YDBqnuiaqs/giphy.gif',
+                'https://media.giphy.com/media/xUPGcChZRE8p2djeiQ/giphy.gif',
+                'https://media.giphy.com/media/za5xikuRr0OzK/giphy.gif',
+                'https://media.giphy.com/media/26DN4DJqhltqNpT9K/giphy.gif',
+                'https://media.giphy.com/media/LGY967AFmrueY/giphy.gif'
+
+                ]
+            const randNum = Math.floor(Math.random() * gifArray.length);
+            const gifUrl = gifArray[randNum];
+            setMainGif(gifUrl);
+        }
+        catch (error){
+            console.warn(error);
+        }
+    }
+
+    // Picks a random sentence with the weather description 
     async function getDesc(data){
         try{
             const weatherType = data.weather[0].description
@@ -96,10 +136,10 @@ const App = () => {
     return (
         <div className="main-container">
             <div className="main-header">
-                <h2>Welcome to Temp-er.</h2>
-                <h3>Search for a city below!</h3>
+                <h2>welcome to temp-er!</h2>
+                <h3>what's the weather like in...</h3>
+                <input type="text" className="search" placeholder="wellington, nz" spellCheck={true} value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
             </div>
-            <input type="text" className="search" placeholder="Wellington, NZ" value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
             {weather.main && (
                 <div className="city">
                     <h2 className="city-name">
@@ -116,6 +156,17 @@ const App = () => {
                     </div>
                 </div>
             )}
+            <div className="surprise">
+                <a href="https://giphy.com/gifs/arielle-m-coming-soon-3o72FkiKGMGauydfyg/fullscreen" target="_parent">
+                    surprise me!
+                </a>
+            </div>
+            <div className="back">
+                <a href="default.asp" target="_parent">
+                    try another?
+                </a>
+            </div>
+            
         </div>
     );
 }
